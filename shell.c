805d19e6bd944a3886d49a1e50c7cc652447d545
SHIYING <szheng@oberlin.edu>
2013-12-07 21:46:53 -0500

    Initialized shell stuff

---
+++ shell.c
@@ -0,0 +1,74 @@
+/*Shiying Zheng and Ben Stern hw10*/
+
+#include "shell.h"
+
+char** parse(){
+	char* line=malloc(sizeof(char)*(LINE_MAX+1));
+	line=fgets(line, LINE_MAX+1, stdin);
+	char** parsed=malloc(sizeof(char*)*(TOK_MAX+1));
+	char* cur=malloc(sizeof(char)*(LINE_MAX+1));
+	int counter=0;
+	cur[0]=0;
+	line[0]=0;
+	parsed[0]=0;
+
+	if (!line) perror("Error when reading line");
+
+	while (line){
+		char c=line[0];
+		if (c=='\n'){
+			break;
+		}
+		else if ( c==' ' ){
+			parsed[counter]=cur;
+			counter++;
+			cur[0]=0;
+		}
+		else if( c=='<' || c=='>' || c=='|' || c=='&'){
+			parsed[counter]=cur;
+			cur[0]=0;
+			counter++;
+			parsed[counter]=&c;
+			counter++;
+		}
+		else {
+			int i=strlen(cur);
+			cur[i]=c;
+			cur[i+1]=0;		}
+		line++;
+	}
+
+	free(cur);
+	free(line);
+
+	return parsed;
+}
+
+int main() {
+	char** string=parse();
+	printf("%s",*string);
+	free(string);
+/*
+    pid_t child;
+    child = fork();
+    int c;
+	
+	if ( -1 == child ) {
+    	    perror("Fork failed");
+       		return(1);
+    }
+
+	while ((c=getchar())!=EOF){
+
+		if (child){//if I have a child then I'm the parent
+			int status;
+        	pid_t deadchild = wait(&status);
+        	exit(0);
+		}
+		else{//I'm the child
+			char *argv[3] = {"ls", ".", NULL};
+			execvp("ls",argv);
+		}
+	}
+	*/
+}
---
+++ shell.h
@@ -0,0 +1,12 @@
+/*Shiying Zheng and Ben Stern hw10*/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define LINE_MAX 1024
+#define TOK_MAX 100
